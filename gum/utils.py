5e6d15bbcbb963446cd6c292bd90e3e61381a118
Ido Barel <55856610+idobarel@users.noreply.github.com>
2023-06-06 17:50:18 +0300

    Update utils.py
    New features included

--- gum/utils.py
+++ gum/utils.py
@@ -1,39 +1,43 @@
 from os import popen
 from os import system
 
-def choose(msg:str, opt:list["str"], type:object=str):
+
+def choose(msg: str, opt: list["str"], type: object = str):
     """A drop down menu.\n
     takes 2 arguments & 1 potential.
     msg >> the question you want to ask the user.
     opt >> the options you want to give the user. as a list of strings*
     *type >> the return type. a string or a number typed obj.
-    
+
     Althogh the type is different, the list must be a list of
     strings.
     """
     print(msg)
     s = " ".join(opt)
-    return type(popen("gum choose "+s).read().strip())
+    return type(popen("gum choose " + s).read().strip())
 
-def input(placeholder:str="")->str:
+
+def input(placeholder: str = "", is_password=True, header="", prompt="") -> str:
     """
     Regular input, with a potetial placeholder.
     """
-    if placeholder != "":
-        x = popen(f"gum input --placeholder {placeholder}").read().strip()
+    if not is_password:
+        x = popen(f"gum input --placeholder '{placeholder}' --header '{header}' --prompt '{prompt}'").read().strip()
     else:
-        x = popen("gum input").read()
+        x = popen(f"gum input --placeholder '{placeholder}' --header '{header}' --prompt '{prompt}' --password").read().strip()
     return x
 
-def confirm(msg:str)->bool:
+
+def confirm(msg: str, background_color=212, color=255) -> bool:
     """
     A yes or no question box, will return
     True on yes, False on no.
     """
-    x = system(f'gum confirm "{msg}"')
+    x = system(f'gum confirm "{msg}" --selected.background {background_color} --selected.foreground {color}')
     return x == 0
 
-def write()->str:
+
+def write() -> str:
     """
     Let you write multi-lined text,
     CTRL+C to stop, and recive the input.
@@ -41,7 +45,8 @@ def write()->str:
     x = popen("gum write").read()
     return x
 
-def search( options:list[str]):
+
+def search(options: list[str]):
     """
     Opens up a search box in your terminal,
     where the user can search from your options.
@@ -54,21 +59,21 @@ def search( options:list[str]):
     popen("rm temp")
     return x
 
-def spin(title:str, time:int=3, spinner:str="dot"):
+
+def spin(title: str, time: int = 3, spinner: str = "dot"):
     """
-Printing a spinner with a title
-types of spinners:\ndot,
-line,
-minidot,
-jump,
-pulse,
-points,
-globe,
-moon,
-monkey,
-meter,
-hamburger
-"""
+    Printing a spinner with a title
+    types of spinners:\ndot,
+    line,
+    minidot,
+    jump,
+    pulse,
+    points,
+    globe,
+    moon,
+    monkey,
+    meter,
+    hamburger"""
     spinners = [
         "dot",
         "line",
@@ -80,8 +85,14 @@ hamburger
         "moon",
         "monkey",
         "meter",
-        "hamburger"
-                 ]
+        "hamburger",
+    ]
     if spinner not in spinners:
         raise ValueError(f"The spinner {spinner} was not found...")
     system(f'gum spin --title "{title}" --spinner "{spinner}" sleep {time}')
+
+
+def bunner(text: str, border: str, margin: int, padding: tuple, color: int = 212):
+    system(
+        f"""gum style --border {border} --margin "{margin}" --padding "{padding[0]} {padding[1]}" --border-foreground {color} "{text}" """
+    )
